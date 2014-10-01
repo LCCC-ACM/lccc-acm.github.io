f5380a8f647d939eff985f7cd2ee3d62801082d4
Mike Koch <mkoch227@gmail.com>
2014-10-01 18:47:52 -0400

    Contact modal is now working on the website

--- js/email.js
+++ js/email.js
@@ -1,11 +1,11 @@
 $(function(){
   
-  $('#sendit').click(function(){
-    var fromName = $("#fromName").val();
-    var fromEmail = $("#fromEmail").val();
-    var message = '<p>' + $("#emailMessage").val() + '</p>';
+  $('#contactsubmit').click(function(){
+    var fromName = $("#name").val();
+    var fromEmail = $("#email").val();
+    var message =  $("#message").val() + '</p>';
     var emailHeader = '<img src=\"http://lccc-acm.github.io/images/lccc-acm.png\"><br><h3>New Message From the LCCC-ACM Website</h3><br>';
-    var emailFromInformation = '<p><b>From: </b>' + fromName + " (" + fromEmail + ")</p><p><b>Message: </b></p>";
+    var emailFromInformation = '<p><b>From: </b>' + fromName + " (" + fromEmail + ")</p><p><b>Message: </b>";
     var emailFooter = '<br><br><p>&copy;2014 LCCC-ACM</p>';
     var htmlFormattedEmail = emailHeader + emailFromInformation + message + emailFooter;
       
@@ -29,11 +29,11 @@ $(function(){
     });
     
     xhr.done(function(data) {
-      $('#console').append(JSON.stringify(data));
+      $('#feedback').addClass('alert').addClass('alert-success').append('<i class="fa fa-check-circle"></i> Your message was successfully sent!');
     });
     
     xhr.fail(function(jqXHR, textStatus, errorThrown) {
-      $('#console').append(jqXHR.responseText);
+      $('#feedback').addClass('alert').addClass('alert-danger').append('<i class="fa fa-times-circle"></i> An error occurred when trying to send your contact request. Please try again later');
     });
     
   });
